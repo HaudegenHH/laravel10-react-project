a21d885a8010b36036c0a22c8004d06c9faccbe2
HaudegenHH <einsundeins79@gmx.de>
2023-06-26 23:39:02 +0200

    scaffold Login page

--- react/src/contexts/ContextProvider.jsx
+++ react/src/contexts/ContextProvider.jsx
@@ -8,9 +8,12 @@ const StateContext = createContext({
 });
 
 export const ContextProvider = ({ children }) => {
-  const [user, setUser] = useState({});
-  // const [token, _setToken] = useState(123);
-  const [token, _setToken] = useState(localStorage.getItem('ACCESS_TOKEN'));
+  const [user, setUser] = useState({
+    name: 'Bob'
+  });
+  // const [user, setUser] = useState({});
+  const [token, _setToken] = useState(123);
+  // const [token, _setToken] = useState(localStorage.getItem('ACCESS_TOKEN'));
 
   const setToken = (token) => {
     _setToken(token);
--- react/src/views/Login.jsx
+++ react/src/views/Login.jsx
@@ -1,8 +1,23 @@
 
 export default function Login() {
+
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+  }
+
   return (
-    <div>
-      Login
+    <div className="login-signup-form animated fadeInDown">
+      <div className="form">
+        <form onSubmit={onSubmit}>
+          <input type="email" placeholder="Email.." />
+          <input type="password" placeholder="Password.." />
+          <button className="btn btn-block">Login</button>
+          <p className="message">
+            Not registered? <Link to="/signup">Create an account</Link>
+          </p>        
+        </form>
+      </div>
     </div>
   )
 }
